abb7f14a822f8d4f967ec5969dfbdfaf5d7d2792
Supritha v <142386193+Supritha-v@users.noreply.github.com>
2023-12-01 12:44:28 +0530

    Add files via upload

---
+++ WEB/label.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+public class JScrollPaneDemo extends JApplet
+{
+  public void init()
+  {
+     Container contentPane=getContenrPane();
+     ImageIcon ii=new ImageIcon("frame.gif");
+     JLabel j1=new JLabel("france"ii,JLabel.CENTER);
+  }
+}
